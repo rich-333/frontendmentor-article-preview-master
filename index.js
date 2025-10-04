c66d8295401aec0f15dc0e563fe27fcfdf6d1e54
richard <TU_USUARIO@users.noreply.github.com>
2025-10-04 19:55:18 -0400

    Se agregaron algunos cambios para mejorar la pagina
    Se agrego un atributo alt para las imagenes de las redes sociales, esto es para los ayudar a los lectores de pantalla.
    Se agrego codigo en js para hacer que los colores del boton e icono de compartir cambien al momento de hacer click.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const openShare = document.getElementById('open-share');
-const sectionSocial =  document.querySelector('.social');
+const openShare = document.getElementById("open-share");
+const sectionSocial = document.querySelector(".social");
+const iconShare = document.querySelector(".icon__share");
+const buttoShare = document.querySelector(".profile__share");
 
-openShare.addEventListener('click', () => {
-  sectionSocial.classList.toggle('social__active')
-})
+openShare.addEventListener("click", () => {
+  sectionSocial.classList.toggle("social__active");
+  iconShare.classList.toggle("icon__share-active");
+  buttoShare.classList.toggle("profile__share-active");
+});
